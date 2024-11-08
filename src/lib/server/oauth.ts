920c4bd626bb54f6b6c0c6b8c51defc733ad3a20
R David L Campbell <162393100+WTLdcampbell@users.noreply.github.com>
2024-11-07 20:22:13 -0500

    added important comment for `/lib/server/oauth.ts`

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -1,5 +1,5 @@
 import { GitHub } from "arctic";
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from "$env/static/private";
 
-// TODO: Update redirect URI
+// TODO: Update redirect URI - IMPORTANT: this overrides GitHub OAuth App settings in GitHub
 export const github = new GitHub(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, "http://localhost:5174/login/github/callback");
